4f95553ccc0525b014b2001476abb2d9b678d2d3
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-09-12 14:48:53 +0300

    fix

--- src/Cookie.php
+++ src/Cookie.php
@@ -237,8 +237,7 @@ final class Cookie
         }
 
         // Can be replaced with DateTimeImmutable::createFromInterface in PHP 8.
-        // Returns null on `setTimestamp()` failure.
-        return (new DateTimeImmutable())->setTimestamp($this->expires->getTimestamp()) ?: null;
+        return (new DateTimeImmutable())->setTimestamp($this->expires->getTimestamp());
     }
 
     /**
