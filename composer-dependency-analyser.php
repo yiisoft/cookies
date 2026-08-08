3e1c18927bd7cb31ac3bd49853b28abadb7a11cb
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 17:33:54 +0300

    Add Composer Dependency Analyser (#85)

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/src', false)
+    ->addPathToScan(__DIR__ . '/tests', true)
+    // Virtual "provide"-only package (fulfilled by httpsoft/http-message), never referenced by class name.
+    ->ignoreErrorsOnPackage('psr/http-message-implementation', [ErrorType::UNUSED_DEPENDENCY])
+    // Optional dependency: only referenced via nullable type hints, not required at runtime.
+    ->ignoreErrorsOnPackage('psr/clock', [ErrorType::DEV_DEPENDENCY_IN_PROD]);
